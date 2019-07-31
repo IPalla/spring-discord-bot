e67b51892cd38df61b1725fb4a310ae60482dee6
Ignacio <nachopallaj@gmail.com>
2019-07-31 20:48:12 +0200

    Initial commit

---
+++ src/main/java/ipj/discord/csgo/discordcsgoapi/SpringBootDiscordCsgoApplication.java
@@ -0,0 +1,13 @@
+package ipj.discord.csgo.discordcsgoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiscordCsgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDiscordCsgoApplication.class, args);
+	}
+
+}
